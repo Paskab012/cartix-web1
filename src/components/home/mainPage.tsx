642084c295ba991fbdf0529366b5dba28fbf632d
paskab012 <pascalkabikampan@gmail.com>
2022-05-08 20:41:35 +0200

    Add maiPage components to the project

--- src/components/home/mainPage.tsx
+++ src/components/home/mainPage.tsx
@@ -6,13 +6,27 @@ import Nav from '../header/Navigation';
 const Container = styled.div`
     height: calc(100vh - 85px);
     background-color: red;
+    display: flex;
+    padding: 20px;
+`;
+
+const Left = styled.div`
+    width: 40%;
+    font-
+`;
+
+const Right = styled.div`
+    width: 60%;
 `;
 
 export const MainPage = () => {
     return (
         <>
             <Nav />
-            <Container>Introduction</Container>
+            <Container>
+                <Left>LLLL</Left>
+                <Right>rrr</Right>
+            </Container>
         </>
     );
 };
