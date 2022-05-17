68f922261c66dbbb0a25ef7d4b9ae3a2581b6a58
kandy-peter <kandypeter03@gmail.com>
2022-05-17 13:28:56 +0200

    implement tooltip functionalities

---
+++ src/components/map/rwandaCountry/tooltip/tooltip.tsx
@@ -0,0 +1,52 @@
+
+import React, { useState, useRef } from 'react'
+import classNames from 'classnames';
+import TooltipContent from './tooltipContent'
+
+const Tooltip = ({children: tooltipTarget, tooltipContent}) => {
+  const targetRef = useRef(null)
+  const [show, setShow] = useState(false)
+
+  const [toolTipProperties, setToolTipProperties] = useState({
+    tooltipTop: 0,
+    tooltipLeft: 0,
+  })
+
+  const handleShowToolTip = () => {
+    const toolTipHalfWidth =
+      targetRef.current.getBoundingClientRect().width / 2
+    const toolTipHeight =
+      targetRef.current.getBoundingClientRect().height
+
+    setToolTipProperties({
+      tooltipTop: toolTipHeight,
+      tooltipLeft: toolTipHalfWidth,
+    })
+
+    setShow(true)
+  }
+
+  return (
+    <div
+      ref={targetRef}
+      onMouseEnter={handleShowToolTip}
+      onMouseLeave={() => setShow(false)}
+      className={classNames(
+        show && 'pb-6', // Make sure tooltip doesn't close when moving cursor to content
+        "inline relative"
+      )}
+    >
+      {/* Putting content inside this div allows it to stay open when hovering over content */}
+      <TooltipContent
+        show={show}
+        top={toolTipProperties.tooltipTop}
+        left={toolTipProperties.tooltipLeft}
+      >
+        {tooltipContent}
+      </TooltipContent>
+      <span className="cursor-pointer">{tooltipTarget}</span>
+    </div>
+  )
+}
+
+export default Tooltip;
---
+++ src/components/map/rwandaCountry/tooltip/tooltipDetails.tsx
@@ -0,0 +1,11 @@
+const TooltipDetails = () => (
+  <ul>
+    <li>New Stats</li>
+    <li>New Stats</li>
+    <li>New Stats</li>
+    <li>New Stats</li>
+    <li>New Stats</li>
+  </ul>
+)
+
+export default TooltipDetails
