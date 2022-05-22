d6b276070d0e2c3c8187f7321bb60bcbcfccb896
Pascal <pascal@Pascals-MacBook-Pro.local>
2022-05-22 13:16:02 +0200

    Add pictures to the signup component

--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.image`
+  background-image: linear-gradient();
+`
 
 const SignUp = () => {
   return (
