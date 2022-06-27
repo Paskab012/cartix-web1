07c7a6747e2b797764a87f7488da78ae40712827
Lucien Makutano <t-lmakutano@microsoft.com>
2022-06-27 23:28:21 +0300

    Fix webpack resolve.fallback error

--- craco.config.js
+++ craco.config.js
@@ -7,9 +7,14 @@ module.exports = {
     webpack: {
         configure: (webpackConfig, { env, paths }) => {
             // eslint-disable-next-line no-param-reassign
-            webpackConfig.resolve.fallback = {
-                fs: false,
-            };
+            // webpackConfig.resolve.fallback = {
+            //     fs: false,
+            // };
+            webpackConfig.module.rules.push({
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: "javascript/auto"
+            });
             return webpackConfig;
         },
     },
