19fc580e44919b233da7759ed217b1bdd965626c
Lucien Makutano <t-lmakutano@microsoft.com>
2022-06-29 10:03:18 +0300

    Fix typos and errors

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import MapHeader from './header/MapHeader';
 import MapNavbar from './MapHeader';
-import { Rwanda } from './rwandaCountry/Rwanda';
+// import { Rwanda } from './rwandaCountry/Rwanda';
 
 function Map() {
     return (
